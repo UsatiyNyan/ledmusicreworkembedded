abdf00cca1e341114604709df85aa9d6ab69c414
UsatiyNyan <kirillarionov123456@gmail.com>
2020-04-29 22:53:36 +0300

    serial

--- lib/serial/include/serial.h
+++ lib/serial/include/serial.h
@@ -1,21 +1,20 @@
 #ifndef __ARDUINO_SERIAL_LIB_H__
 #define __ARDUINO_SERIAL_LIB_H__
 
-#include <cstdint>   // Standard types
-#include <cstdio>    // Standard input/output definitions
-#include <unistd.h>   // UNIX standard function definitions
-#include <fcntl.h>    // File control definitions
-#include <cerrno>    // Error number definitions
-#include <termios.h>  // POSIX terminal control definitions
-#include <cstring>   // String function definiitions
-#include <sys/ioctl.h>
-#include <stdint.h>
+#include "file_descriptor.h"
+#include <string>
 
-int serialport_init(const char *serialport, int baud);
-int serialport_close(int fd);
-int serialport_writebyte(int fd, uint8_t b);
-int serialport_write(int fd, const char *str);
-int serialport_read_until(int fd, char *buf, char until, int buf_max, int timeout);
-int serialport_flush(int fd);
-
+namespace serial {
+class Connection {
+ public:
+    Connection(const std::string &serialport, int baud);
+    size_t write(const void *data, size_t size);
+    void write_exact(const void *data, size_t size);
+    size_t read(void *data, size_t size);
+    void read_exact(void *data, size_t size);
+    void flush();
+ private:
+    fd::FileDescriptor _fd;
+};
+}  // namespace serial
 #endif
--- lib/serial/src/serial.cpp
+++ lib/serial/src/serial.cpp
@@ -1,136 +1,116 @@
 #include "serial.h"
+#include "base_exception.h"
 
-// uncomment this to debug reads
-//#define SERIALPORTDEBUG
+//#include <cstdio>    // Standard input/output definitions
+#include <unistd.h>   // UNIX standard function definitions
+#include <fcntl.h>    // File control definitions
+#include <termios.h>  // POSIX terminal control definitions
+
+using exception::Exception;
+
+
+namespace serial {
+static uint32_t int_to_baud(int baud) {
+    switch (baud) {
+        case 4800:return B4800;
+        case 9600:return B9600;
+#ifdef B14400
+        case 14400:return B14400;
+#endif
+        case 19200:return B19200;
+#ifdef B28800
+        case 28800:return B28800;
+#endif
+        case 38400:return B38400;
+        case 57600:return B57600;
+        case 115200:return B115200;
+        default: throw Exception("wrong baud number");
+    }
+}
 
 // takes the string name of the serial port (e.g. "/dev/tty.usbserial","COM1")
 // and a baud rate (bps) and connects to that port at that speed and 8N1.
 // opens the port in fully raw mode so you can send binary data.
-// returns valid fd, or -1 on error
-int serialport_init(const char *serialport, int baud) {
-    struct termios toptions;
-    int fd;
-
-    //fd = open(serialport, O_RDWR | O_NOCTTY | O_NDELAY);
-    fd = open(serialport, O_RDWR | O_NONBLOCK);
-
-    if (fd == -1) {
-        perror("serialport_init: Unable to open port ");
-        return -1;
+Connection::Connection(const std::string &serialport, int baud) {
+    termios tty{};
+    //file_descriptor = open(serialport, O_RDWR | O_NOCTTY | O_NDELAY);
+    _fd = fd::FileDescriptor(open(serialport.c_str(), O_RDWR | O_NONBLOCK));
+    if (_fd.fd() == -1) {
+        throw Exception("serialport_init: Unable to open port ");
     }
 
     //int iflags = TIOCM_DTR;
-    //ioctl(fd, TIOCMBIS, &iflags);     // turn on DTR
-    //ioctl(fd, TIOCMBIC, &iflags);    // turn off DTR
+    //ioctl(file_descriptor, TIOCMBIS, &iflags);     // turn on DTR
+    //ioctl(file_descriptor, TIOCMBIC, &iflags);    // turn off DTR
 
-    if (tcgetattr(fd, &toptions) < 0) {
-        perror("serialport_init: Couldn't get term attributes");
-        return -1;
+    if (tcgetattr(_fd.fd(), &tty) < 0) {
+        throw Exception("serialport_init: Couldn't get term attributes");
     }
-    speed_t brate = baud; // let you override switch below if needed
-    switch (baud) {
-        case 4800: brate = B4800;
-            break;
-        case 9600: brate = B9600;
-            break;
-#ifdef B14400
-            case 14400:  brate=B14400;  break;
-#endif
-        case 19200: brate = B19200;
-            break;
-#ifdef B28800
-            case 28800:  brate=B28800;  break;
-#endif
-        case 38400: brate = B38400;
-            break;
-        case 57600: brate = B57600;
-            break;
-        case 115200: brate = B115200;
-            break;
-    }
-    cfsetispeed(&toptions, brate);
-    cfsetospeed(&toptions, brate);
+    speed_t brate = int_to_baud(baud); // let you override switch below if needed
+
+    cfsetispeed(&tty, brate);
+    cfsetospeed(&tty, brate);
 
     // 8N1
-    toptions.c_cflag &= ~PARENB;
-    toptions.c_cflag &= ~CSTOPB;
-    toptions.c_cflag &= ~CSIZE;
-    toptions.c_cflag |= CS8;
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;
     // no flow control
-    toptions.c_cflag &= ~CRTSCTS;
+    tty.c_cflag &= ~CRTSCTS;
 
     //toptions.c_cflag &= ~HUPCL; // disable hang-up-on-close to avoid reset
 
-    toptions.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
-    toptions.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
+    tty.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
 
-    toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
-    toptions.c_oflag &= ~OPOST; // make raw
+    tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
+    tty.c_oflag &= ~OPOST; // make raw
 
     // see: http://unixwiz.net/techtips/termios-vmin-vtime.html
-    toptions.c_cc[VMIN] = 0;
-    toptions.c_cc[VTIME] = 0;
+    tty.c_cc[VMIN] = 0;
+    tty.c_cc[VTIME] = 0;
     //toptions.c_cc[VTIME] = 20;
 
-    tcsetattr(fd, TCSANOW, &toptions);
-    if (tcsetattr(fd, TCSAFLUSH, &toptions) < 0) {
-        perror("init_serialport: Couldn't set term attributes");
-        return -1;
+    tcsetattr(_fd.fd(), TCSANOW, &tty);
+    if (tcsetattr(_fd.fd(), TCSAFLUSH, &tty) < 0) {
+        throw Exception("init_serialport: Couldn't set term attributes");
     }
-
-    return fd;
 }
 
-//
-int serialport_close(int fd) {
-    return close(fd);
-}
-
-//
-int serialport_writebyte(int fd, uint8_t b) {
-    int n = write(fd, &b, 1);
-    if (n != 1)
-        return -1;
-    return 0;
-}
-
-//
-int serialport_write(int fd, const char *str) {
-    int len = strlen(str);
-    int n = write(fd, str, len);
-    if (n != len) {
-        perror("serialport_write: couldn't write whole string\n");
-        return -1;
+size_t Connection::write(const void *data, size_t size) {
+    ssize_t written = ::write(_fd.fd(), data, size);
+    if (written == -1) {
+        throw Exception("write error");
     }
-    return 0;
+    return written;
 }
 
-//
-int serialport_read_until(int fd, char *buf, char until, int buf_max, int timeout) {
-    char b[1];  // read expects an array, so we give it a 1-byte array
-    int i = 0;
-    do {
-        int n = read(fd, b, 1);  // read a char at a time
-        if (n == -1) return -1;    // couldn't read
-        if (n == 0) {
-            usleep(1 * 1000);  // wait 1 msec try again
-            timeout--;
-            if (timeout == 0) return -2;
-            continue;
-        }
-#ifdef SERIALPORTDEBUG
-        printf("serialport_read_until: i=%d, n=%d b='%c'\n",i,n,b[0]); // debug
-#endif
-        buf[i] = b[0];
-        i++;
-    } while (b[0] != until && i < buf_max && timeout > 0);
-
-    buf[i] = 0;  // null terminate the string
-    return 0;
+void Connection::write_exact(const void *data, size_t size) {
+    size_t written = 0;
+    while (size != written) {
+        written += write(static_cast<const char *>(data) + written, size - written);
+    }
 }
 
-//
-int serialport_flush(int fd) {
-    sleep(2); //required to make flush work, for some reason
-    return tcflush(fd, TCIOFLUSH);
+size_t Connection::read(void *data, size_t size) {
+    ssize_t read_n = ::read(_fd.fd(), data, size);
+    if (read_n == -1) {
+        throw Exception("read error");
+    }
+    return read_n;
 }
+
+void Connection::read_exact(void *data, size_t size) {
+    size_t read_n = 0;
+    while (size != read_n) {
+        read_n += read(static_cast<char *>(data) + read_n, size - read_n);
+    }
+}
+
+void Connection::flush() {
+    if (tcflush(_fd.fd(), TCIOFLUSH) == -1) {
+        throw Exception("tcflush");
+    }
+}
+} // namespace serial
