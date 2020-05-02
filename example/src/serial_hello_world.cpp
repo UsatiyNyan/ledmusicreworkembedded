7f87260eb33de38401a360aa7299c92fa7bad114
UsatiyNyan <kirillarionov123456@gmail.com>
2020-05-03 02:30:01 +0300

    test rgb

--- example/src/serial_hello_world.cpp
+++ example/src/serial_hello_world.cpp
@@ -9,7 +9,14 @@ constexpr const char * serial_port  = "/dev/serial0";
 
 int main() {
     serial::Connection connection(serial_port, 19200);
-    std::vector<uint8_t> buf{3, 0};
-    connection.read_exact(buf.data(), 3);
-    std::cout << buf.data();
+    std::vector<uint8_t> buf{128, 0};
+    while (true) {
+        std::cout << connection.read(buf.data(), 128) << std::endl;
+        std::cout <<
+            "c: " << static_cast<int>(buf[0]) << " | "
+            "f: " << static_cast<int>(buf[1]) << " | "
+            "r: " << static_cast<int>(buf[2]) << " | "
+            "g: " << static_cast<int>(buf[3]) << " | "
+            "b: " << static_cast<int>(buf[4]) << std::endl;
+    }
 }
