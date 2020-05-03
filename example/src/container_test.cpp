f2b95a24baf6a5ee203460d538c9cb6bd9127a99
UsatiyNyan <kirillarionov123456@gmail.com>
2020-05-03 19:14:48 +0300

    fixing queue p2

--- example/src/container_test.cpp
+++ example/src/container_test.cpp
@@ -7,8 +7,10 @@
 #include "fixed_queue.h"
 
 int main() {
-    container::FixedQueue<int> fixed_queue(10);
+    container::FixedQueue<int> fixed_queue(2);
     fixed_queue.push_back(1);
+    fixed_queue.push_back(2);
+    fixed_queue.push_back(3);
     std::cout << fixed_queue.back();
     return 0;
 };
